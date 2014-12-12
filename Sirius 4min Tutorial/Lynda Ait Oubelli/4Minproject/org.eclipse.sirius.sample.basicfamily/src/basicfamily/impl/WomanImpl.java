4330006ffc7fbdded5322a15b21bc40c79291bee
lyndaait <aitoubellilynda@gmail.com>
2014-12-12 13:19:39 +0100

    "ajout de 4minits tutorial try"

---
+++ Sirius 4min Tutorial/Lynda Ait Oubelli/4Minproject/org.eclipse.sirius.sample.basicfamily/src/basicfamily/impl/WomanImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package basicfamily.impl;
+
+import basicfamily.BasicfamilyPackage;
+import basicfamily.Woman;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Woman</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class WomanImpl extends PersonImpl implements Woman {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected WomanImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return BasicfamilyPackage.Literals.WOMAN;
+	}
+
+} //WomanImpl
