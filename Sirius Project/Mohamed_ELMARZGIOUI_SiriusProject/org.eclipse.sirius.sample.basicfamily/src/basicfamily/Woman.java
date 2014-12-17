1bcfface45ac3f475fc8a6cd20ccf1c9e928266b
Mohamed EL MARZGIOUI <E14C645L@S046V2pc03.ensinfo.sciences.univ-nantes.prive>
2014-12-17 13:13:51 +0000

    siriusProject

---
+++ Sirius Project/Mohamed_ELMARZGIOUI_SiriusProject/org.eclipse.sirius.sample.basicfamily/src/basicfamily/Woman.java
@@ -0,0 +1,17 @@
+/**
+ */
+package basicfamily;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Woman</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see basicfamily.BasicfamilyPackage#getWoman()
+ * @model
+ * @generated
+ */
+public interface Woman extends Person {
+} // Woman
